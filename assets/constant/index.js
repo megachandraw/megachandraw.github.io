02fac8cb9f83d799061574a62c018209bfb246db
unknown <chandramega96@gmail.com>
2024-05-26 20:14:43 +0700

    update data education and experience

--- assets/constant/index.js
+++ assets/constant/index.js
@@ -10,11 +10,11 @@ var portfolio = {
   bio: {
     en: {
       title: "My Bio",
-      text: "Saya",
+      text: "I am an Information Systems graduate who has work experience as an IT staff where I am responsible for creating systems and managing internal systems such as POS and ERP in companies. Currently I am interested in the field of AI / ML Engineer",
     },
     id: {
       title: "Bio",
-      text: "Saya lulusan Sistem Informasi yang memiliki pengalaman pekerjaan sebagai IT Staff dimana bertangungg jawab dalam pembuatan",
+      text: "Saya lulusan Sistem Informasi yang memiliki pengalaman pekerjaan sebagai IT staff dimana bertanggung jawab dalam pembuatan sistem dan mengelola sistem internal seperti POS dan ERP di perusahaan. Saat ini saya tertarik di bidang AI / ML Engineer",
     },
   },
   exp: {
@@ -23,31 +23,84 @@ var portfolio = {
       text: [
         {
           jobTitle: "IT Staff",
-          comp: "PT. Galuh Citarum",
-          date: "2021 - present",
-          jobdesk: [
-            "Mengatur Kewenangan hak akses user dalam sistem internal seperti POS System dan ERP",
-            "Menjadi liaison antara user dengan vendor dalam menganalisa kendala yang dihadapi oleh end user.",
-            "Membuat program internal untuk perusahaan.",
-          ],
+          comp: " PT. Galuh Citarum",
+          date: "Jan 2021 - present"
         },
         {
           jobTitle: "HRGA Staff",
-          comp: "PT. DaikyoNishikawa Tenma Indonesia",
-          date: "2018 - 2020",
-          jobdesk: [
-            "Mengatur Kewenangan hak akses user dalam sistem internal seperti POS System dan ERP",
-            "Menjadi liaison antara user dengan vendor dalam menganalisa kendala yang dihadapi oleh end user.",
-            "Membuat program internal untuk perusahaan.",
-          ],
+          comp: " PT. DaikyoNishikawa Tenma Indonesia",
+          date: "Nov 2018 - Okt 2020"
+        },
+        {
+          jobTitle: "Production Operator",
+          comp: " PT. HPPM",
+          date: "Agst 2017 - Agst 2018"
+        },
+        {
+          jobTitle: "Production Operator",
+          comp: " PT. Shin-Etsu Polymer Indonesia",
+          date: "Apr 2015 - Apr 2017"
         },
       ],
     },
     id: {
       title: "Pengalaman",
-      text: "",
+      text:  [
+        {
+          jobTitle: "IT Staff",
+          comp: " PT. Galuh Citarum",
+          date: "Jan 2021 - present"
+        },
+        {
+          jobTitle: "HRGA Staff",
+          comp: " PT. DaikyoNishikawa Tenma Indonesia",
+          date: "Nov 2018 - Okt 2020"
+        },
+        {
+          jobTitle: "Operator Produksi",
+          comp: " PT. HPPM",
+          date: "Agustus 2017 - Agustus 2018"
+        },
+        {
+          jobTitle: "Operator Produksi",
+          comp: " PT. Shin-Etsu Polymer Indonesia",
+          date: "Apr 2015 - Apr 2017"
+        },
+      ],
     },
   },
+  education : {
+    en : {
+      title : "Education",
+      text : [
+      {
+          name : "STMIK Kharisma Karawang",
+          major : "Information System",
+          date: "Sept 2016 - Sept 2020"
+      },
+      {
+          name : "SMAN 5 Karawang",
+          major : "Science",
+          date: " Mei 2011 - Mei 2014"
+      },
+      ]
+    },
+    id : {
+      title : "Pendidikan",
+      text : [
+      {
+          name : "STMIK Kharisma Karawang",
+          major : "Sistem Informasi",
+          date: "Sept 2016 - Sept 2020"
+      },
+      {
+          name : "SMAN 5 Karawang",
+          major : "IPA",
+          date: " Mei 2011 - Mei 2014"
+      },
+      ]
+    },
+  }
 };
 
 // Fungsi untuk mengubah bahasa
@@ -58,12 +111,14 @@ function changeLanguage() {
   document.getElementById("textButton").textContent =
     portfolio.textButton[selectedLanguage];
   document.getElementById("bioTitle").textContent =
-    portfolio.bio[selectedLanguage]["title"];
-  document.getElementById("bio").textContent = portfolio.bio[selectedLanguage];
+    portfolio.bio[selectedLanguage].title;
+  document.getElementById("bio").textContent = portfolio.bio[selectedLanguage].text;
   document.getElementById("experienceTitle").textContent =
     portfolio.exp[selectedLanguage]["title"];
 
   var expContainer = document.getElementById("expContainer");
+  expContainer.innerHTML = '';
+
   var experience = portfolio.exp[selectedLanguage].text;
   experience.forEach(function (job) {
     var qualificationData = document.createElement("div");
@@ -98,6 +153,49 @@ function changeLanguage() {
 
     // Menambahkan elemen kualifikasi ke dalam container
     expContainer.appendChild(qualificationData);
+  });
+
+
+  document.getElementById("eduTitle").textContent =
+    portfolio.education[selectedLanguage]["title"];
+
+  var eduContainer = document.getElementById("eduContainer");
+  eduContainer.innerHTML = '';
+
+  var edu = portfolio.education[selectedLanguage].text;
+  edu.forEach(function (job) {
+    var qualificationData = document.createElement("div");
+    qualificationData.classList.add("qualification__data");
+
+    // Menambahkan judul area kualifikasi (judul pekerjaan)
+    var qualificationArea = document.createElement("h3");
+    qualificationArea.classList.add("qualification__area");
+    qualificationArea.textContent = job.name;
+    qualificationData.appendChild(qualificationArea);
+
+    // Membuat kotak untuk informasi pekerjaan
+    var qualificationBox = document.createElement("div");
+    qualificationBox.classList.add("qualification__box");
+
+    // Menambahkan informasi pekerjaan: perusahaan
+    var workCompany = document.createElement("span");
+    workCompany.classList.add("qualification__work");
+    workCompany.innerHTML =
+      "<i class='bx bx-briefcase-alt qualification__icon'></i> " + job.major;
+    qualificationBox.appendChild(workCompany);
+
+    // Menambahkan informasi pekerjaan: periode
+    var workDate = document.createElement("span");
+    workDate.classList.add("qualification__work");
+    workDate.innerHTML =
+      "<i class='bx bx-calendar-alt qualification__icon'></i>" + job.date;
+    qualificationBox.appendChild(workDate);
+
+    // Menambahkan kotak informasi pekerjaan ke dalam data kualifikasi
+    qualificationData.appendChild(qualificationBox);
+
+    // Menambahkan elemen kualifikasi ke dalam container
+    eduContainer.appendChild(qualificationData);
   });
 }
 
